42314bfcd2aa1d68001b0d7d2220f1bbf7033ebd
PrathamSakhare <psakhre@themohh.com>
2023-06-29 15:06:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TopBar from './components/TopBar';
-import MainPage from './components/MainPage';
-import Navbar from './components/Navbar';
-import PageSecond from './components/PageSecond';
-import PageThird from './components/PageThird';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TopBar from "./components/TopBar";
+import MainPage from "./components/MainPage";
+import Navbar from "./components/Navbar";
+import PageSecond from "./components/PageSecond";
+import PageThird from "./components/PageThird";
 function App() {
   return (
     <>
-    
-    <div className="App">
-      <div className='upperSection'>
-      <TopBar />
-      <Navbar />
-    </div>
-    <div id='modelS'>
-
-        <MainPage />
-    </div>
-    <div id='model3'>
-
-        <PageSecond />
-    </div>
-    <div id='modelY'>
-
-        <PageThird />
-    </div>
-      
-    </div>
+      <div className="App">
+        <div className="upperSection">
+          <TopBar />
+          <Navbar />
+        </div>
+        <div id="modelS">
+          <MainPage />
+        </div>
+        <div id="model3">
+          <PageSecond />
+        </div>
+        <div id="modelY">
+          <PageThird />
+        </div>
+      </div>
     </>
   );
 }
